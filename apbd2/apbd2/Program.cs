4f532da506ad656dc47f5087780cd9f166c64f5e
oli <s28526@pjwstk.edu.pl>
2024-03-24 23:42:24 +0100

    utworzenie klasy Container oraz zbudowanie konstruktora. utworzenie enuma do wyboru odpowiedniego kontenera

--- apbd2/apbd2/Program.cs
+++ apbd2/apbd2/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+public enum ContainerType
+{
+    Chlodniczy,
+    Plynny,
+    Gazowy
+}
+
+class Container
+{
+    private static int ContainerSerialNumber = 1;
+    
+    public string SerialNumber { get; private set;}
+    public double PackageMassInKg { get; private set; }
+    public double Height { get; private set; }
+    public double OwnMass { get; private set; }
+    public double Depth { get; private set; }
+    
+    public ContainerType Type { get; private set; }
+
+
+    Container(ContainerType type, string serialNumber, double packageMassInKg, double ownMass, double height,
+        double depth)
+    {
+        Type = type;
+        SerialNumber = serialNumber;
+        PackageMassInKg = packageMassInKg;
+        OwnMass = ownMass;
+        Height = height;
+        Depth = depth;
+    }
+}
+
