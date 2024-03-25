3235f1444ca158d2b54598e1f03ee32f6d03f7cf
oli <s28526@pjwstk.edu.pl>
2024-03-25 20:07:02 +0100

    klasa chlodniczyContainer

---
+++ apbd2/apbd2/ChlodniczyContainer.cs
@@ -0,0 +1,35 @@
+﻿namespace apbd2;
+
+public class ChlodniczyContainer : Container, IHazardNotifier
+{
+    public ProductType ProductType { get; set; } 
+    public double Temperature { get; set; }
+
+
+    public ChlodniczyContainer(ProductType productType, double temperature, double packageMassInKg, double ownMass,
+        double height, double depth) : base(ContainerType.gazowy, packageMassInKg, ownMass, height, depth)
+    {
+        ProductType = productType;
+        Temperature = temperature;
+    }
+    
+    
+    public void DangerMessage(string NumberContainer)
+    {
+        Console.WriteLine($"UWAGA!!!!! kontener o numerze {NumberContainer} za malo temperatury");
+    }
+    
+    
+    public void Load(ProductType productTypee, double temperature, double m)
+    {
+        if (productTypee!= ProductType || temperature<Temperature || m > MaximumLoadWeight())
+        {
+            throw new Exception("jakies parametry sa nieprawidlowe:(");
+        }
+        else
+        {
+            PackageMassInKg = m;
+        }
+    }
+    
+}
--- apbd2/apbd2/Program.cs
+++ apbd2/apbd2/Program.cs
@@ -13,6 +13,19 @@ public enum PlynnyType
     zwykly
 }
 
+public enum ProductType
+{
+    Bananas,
+    Chocolate,
+    Fish,
+    Meat,
+    IceCream,
+    FrozenPizza,
+    Cheese,
+    Sausages,
+    Butter,
+    Eggs
+}
 
 
 class Program
